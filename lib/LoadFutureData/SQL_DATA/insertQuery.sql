1ef29bea77af8dd25d16ebd4741a70c044136d8a
Jagaa2023 <jgmobile2017@gmail.com>
2023-08-01 16:45:00 +0800

    2nd added sql insert query

---
+++ lib/LoadFutureData/SQL_DATA/insertQuery.sql
@@ -0,0 +1,49 @@
+
+
+insert into tbprofile (ID, EMAIL, NAME, COLOR)
+values (1, 'Johny Deep0', 'Johny2000@gmail.com', 4281585374);
+
+insert into tbprofile (ID, EMAIL, NAME, COLOR)
+values (2, 'Johny Deep1', 'Johny2001@gmail.com', 4282585310);
+
+insert into tbprofile (ID, EMAIL, NAME, COLOR)
+values (3, 'Johny Deep2', 'Johny2002@gmail.com', 4283585320);
+
+insert into tbprofile (ID, EMAIL, NAME, COLOR)
+values (4, 'Johny Deep3', 'Johny2003@gmail.com', 4284585330);
+
+insert into tbprofile (ID, EMAIL, NAME, COLOR)
+values (5, 'Johny Deep4', 'Johny2004@gmail.com', 4285585340);
+
+insert into tbprofile (ID, EMAIL, NAME, COLOR)
+values (6, 'Johny Deep5', 'Johny2005@gmail.com', 4286585350);
+
+insert into tbprofile (ID, EMAIL, NAME, COLOR)
+values (7, 'Johny Deep6', 'Johny2006@gmail.com', 4287585360);
+
+insert into tbprofile (ID, EMAIL, NAME, COLOR)
+values (8, 'Johny Deep7', 'Johny2007@gmail.com', 4288585370);
+
+insert into tbprofile (ID, EMAIL, NAME, COLOR)
+values (9, 'Johny Deep9', 'Johny2009@gmail.com', 4289585380);
+
+insert into tbprofile (ID, EMAIL, NAME, COLOR)
+values (10, 'Johny Deep10', 'Johny20010@gmail.com', 4280585390);
+
+insert into tbprofile (ID, EMAIL, NAME, COLOR)
+values (11, 'Johny Deep20', 'Johny20020@gmail.com', 4281585300);
+
+insert into tbprofile (ID, EMAIL, NAME, COLOR)
+values (12, 'Johny Deep30', 'Johny20030@gmail.com', 4282581074);
+
+insert into tbprofile (ID, EMAIL, NAME, COLOR)
+values (13, 'Johny Deep40', 'Johny20040@gmail.com', 4283582074);
+
+insert into tbprofile (ID, EMAIL, NAME, COLOR)
+values (14, 'Johny Deep50', 'Johny20050@gmail.com', 4284583074);
+
+insert into tbprofile (ID, EMAIL, NAME, COLOR)
+values (15, 'Johny Deep60', 'Johny20060@gmail.com', 4285584074);
+
+insert into tbprofile (ID, EMAIL, NAME, COLOR)
+values (16, 'Johny Deep70', 'Johny20070@gmail.com', 4286585074);
